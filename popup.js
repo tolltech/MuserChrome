287fce0c60531436cdb5737867c85b201465f049
tolltech <alexandrovpe@gmail.com>
2020-04-28 08:53:37 +0400

    template for cross domain ajax request

--- popup.js
+++ popup.js
@@ -16,3 +16,17 @@ $(document).ready(function () {
         DownloadPlaylistFromActiveTab();
     });
 });
+
+// $.ajax({
+//     type: 'POST',
+//     url: 'https://to.com/postHere.php',
+//     crossDomain: true,
+//     data: '{"some":"json"}',
+//     dataType: 'json',
+//     success: function(responseData, textStatus, jqXHR) {
+//         var value = responseData.someKey;
+//     },
+//     error: function (responseData, textStatus, errorThrown) {
+//         alert('POST failed.');
+//     }
+// });
