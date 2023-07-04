3a64a6b5a12e2dd907d10d1b5f87001857e305c0
tolltech <alexandrovpe@gmail.com>
2023-07-04 20:06:53 +0400

    try to cache dom playlists

--- trigger.js
+++ trigger.js
@@ -1,25 +1,12 @@
 var wasTollMuserInjected;
+var playlistsCache = {};
+var playlistDomChangeEvents = [];
+
 if (!wasTollMuserInjected) {
     wasTollMuserInjected = true;
 
-    function ParsePlayList() {
-        var tracks = $('div.audio_row');
-        var trackInfos = [];
-
-        var playlistName = $('.AudioPlaylistSnippet__title--main').html();
-
-        for (var i = 0; i < tracks.length; ++i) {
-            var track = $(tracks[i]);
-            var artist = track.find('a.artist_link, .audio_row__performer_title a').html();
-            var song = track.find('span.audio_row__title_inner').html();
-            trackInfos.push({ Artist: artist, Song: song });
-        }
-
-        return { Tracks: trackInfos, Playlist: playlistName };
-    }
-
-    function ParsePlayListYandex() {
-        var tracks = $('.page-playlist__tracks-list div.d-track');
+    function ParsePlayListYandex(jqTracks) {
+        var tracks = jqTracks || $('.page-playlist__tracks-list div.d-track');
         var trackInfos = [];
 
         var playlistName = $('input.page-playlist__title').val() ?? $('.page-playlist__title').html();
@@ -27,38 +14,63 @@ if (!wasTollMuserInjected) {
         for (var i = 0; i < tracks.length; ++i) {
             var track = $(tracks[i]);
             var song = (track.find('a.d-track__title').html() ?? track.find('.d-track__title').html()).trim();
+            var orderId = track.attr('data-id');
 
             var artists = $(track).find('.d-track__artists a');
-            var artist = artists.map(function() {return $(this).html();}).toArray().join(', ');
+            var artist = artists.map(function () { return $(this).html(); }).toArray().join(', ');
 
-            trackInfos.push({ Artist: artist, Song: song });
+            trackInfos.push({ Artist: artist, Song: song, OrderId: track.attr('data-id')});
         }
 
         return { Tracks: trackInfos, Playlist: playlistName };
     }
 
     function DownloadPlayList() {
-        var tracks = ParsePlayListYandex();
+        var tracks = GetTracksFromCacheOrHot();
         var date = new Date();
 
         playlistName = tracks.Playlist || date.getFullYear() + '_' + (date.getMonth() + 1) + '_' + date.getDate();
 
-        SendMessageToExtension(DownloadPlayListCallBackEvent, 
+        SendMessageToExtension(DownloadPlayListCallBackEvent,
             {
-                Tracks: tracks.Tracks, 
-                FileName: playlistName + '_' + window.location.host + window.location.pathname + '.json' 
+                Tracks: tracks.Tracks,
+                FileName: playlistName + '_' + window.location.host + window.location.pathname + '.json'
             });
     }
 
-    AddEventListener(DownloadPlayListEvent, DownloadPlayList);    
+    AddEventListener(DownloadPlayListEvent, DownloadPlayList);
 
     AddEventListener(RequestToGetUrlEvent, function () {
-        var tracks = ParsePlayListYandex();
+        alert(playlistDomChangeEvents[0]);
+        var tracks = GetTracksFromCacheOrHot();
         SendMessageToExtension(GoToTolltechEvent, tracks.Tracks);
     });
-    
-    AddEventListener(GetTracksInfoEvent, function () {        
-        var tracks = ParsePlayListYandex(); 
+
+    AddEventListener(GetTracksInfoEvent, function () {
+        var tracks = GetTracksFromCacheOrHot();
         SendMessageToExtension(FoundTracksEvent, tracks.Tracks.length);
+    });
+
+    function GetTracksFromCacheOrHot() {
+        var tracks = ParsePlayListYandex();
+
+        if (playlistsCache[tracks.Playlist]) {
+            
+        }
+
+        return tracks;
+    }
+    
+    function GetTracksFromCache(){
+        var currentTracks = ParsePlayListYandex();
+
+        return {
+            Playlist: tracks.Playlist,
+            Tracks: playlistsCache[tracks.Playlist].map(function () { return JSON.parse(this); }).toArray()
+        };
+    }
+
+    $('.page-playlist__tracks-list').bind('DOMSubtreeModified', function (e) {        
+        playlistDomChangeEvents.push($(e.target).html());        
     });
 }
